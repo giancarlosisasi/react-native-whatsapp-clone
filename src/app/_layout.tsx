89c7ad0877e759b8f465739bad8a2f41af2ff2ec
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2025-07-29 14:43:41 -0500

    feat: setup clerk

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,35 +11,41 @@ import {
 	SafeAreaProvider,
 } from 'react-native-safe-area-context';
 
+function InitialLayout() {
+	return (
+		<SafeAreaProvider initialMetrics={initialWindowMetrics}>
+			<Stack>
+				<Stack.Screen
+					name='index'
+					options={{
+						headerShown: false,
+					}}
+				/>
+				<Stack.Screen
+					name='otp'
+					options={{
+						headerTitle: 'Enter Your Phone Number',
+						headerBackVisible: false,
+					}}
+				/>
+
+				<Stack.Screen
+					name='verify/[phone]'
+					options={{
+						headerTitle: 'Verify Your Phone Number',
+						headerBackVisible: true,
+						headerBackTitle: 'Edit number',
+					}}
+				/>
+			</Stack>
+		</SafeAreaProvider>
+	);
+}
+
 export default function RootLayout() {
 	return (
 		<ClerkProvider tokenCache={tokenCache}>
-			<SafeAreaProvider initialMetrics={initialWindowMetrics}>
-				<Stack>
-					<Stack.Screen
-						name='index'
-						options={{
-							headerShown: false,
-						}}
-					/>
-					<Stack.Screen
-						name='otp'
-						options={{
-							headerTitle: 'Enter Your Phone Number',
-							headerBackVisible: false,
-						}}
-					/>
-
-					<Stack.Screen
-						name='verify/[phone]'
-						options={{
-							headerTitle: 'Verify Your Phone Number',
-							headerBackVisible: true,
-							headerBackTitle: 'Edit number',
-						}}
-					/>
-				</Stack>
-			</SafeAreaProvider>
+			<InitialLayout />
 		</ClerkProvider>
 	);
 }
