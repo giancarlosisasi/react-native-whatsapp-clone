501a3f915cfcde529c48ee85e8e589adef9db4d9
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2025-07-29 00:23:18 -0500

    chore: format

--- scripts/windows-support.ts
+++ scripts/windows-support.ts
@@ -1,8 +1,8 @@
-import fs from "node:fs";
-import path from "node:path";
+import fs from 'node:fs';
+import path from 'node:path';
 
 async function main() {
-	const isWindows = process.platform === "win32";
+	const isWindows = process.platform === 'win32';
 
 	// fix ninja build, windows errors for long paths
 	if (isWindows) {
@@ -24,65 +24,65 @@ async function main() {
 function fixWindowsLongPathsErrors() {
 	const androidBuildGradlePath = path.join(
 		__dirname,
-		"..",
-		"android",
-		"app",
-		"build.gradle",
+		'..',
+		'android',
+		'app',
+		'build.gradle',
 	);
 	const reanimatedBuildGradlePath = path.join(
 		__dirname,
-		"..",
-		"node_modules",
-		"react-native-reanimated",
-		"android",
-		"build.gradle",
+		'..',
+		'node_modules',
+		'react-native-reanimated',
+		'android',
+		'build.gradle',
 	);
 	const workletsBuildGradlePath = path.join(
 		__dirname,
-		"..",
-		"node_modules",
-		"react-native-worklets",
-		"android",
-		"build.gradle",
+		'..',
+		'node_modules',
+		'react-native-worklets',
+		'android',
+		'build.gradle',
 	);
 
 	const androidBuildGradleNewContent = fs.readFileSync(
-		path.join(__dirname, "./templates/expo-android-gradle.txt"),
-		"utf8",
+		path.join(__dirname, './templates/expo-android-gradle.txt'),
+		'utf8',
 	);
 	const reanimatedBuildGradleNewContent = fs.readFileSync(
-		path.join(__dirname, "./templates/react-native-reanimated-gradle.txt"),
-		"utf8",
+		path.join(__dirname, './templates/react-native-reanimated-gradle.txt'),
+		'utf8',
 	);
 	const workletsBuildGradleNewContent = fs.readFileSync(
-		path.join(__dirname, "./templates/react-native-worklets-gradle.txt"),
-		"utf8",
+		path.join(__dirname, './templates/react-native-worklets-gradle.txt'),
+		'utf8',
 	);
 
 	fs.writeFileSync(
 		androidBuildGradlePath,
 		androidBuildGradleNewContent,
-		"utf8",
+		'utf8',
 	);
-	console.info(">> android/app/build.gradle modified successfully");
+	console.info('>> android/app/build.gradle modified successfully');
 	fs.writeFileSync(
 		reanimatedBuildGradlePath,
 		reanimatedBuildGradleNewContent,
-		"utf8",
+		'utf8',
 	);
 	console.info(
-		">> node_modules/react-native-reanimated/android/build.gradle modified successfully",
+		'>> node_modules/react-native-reanimated/android/build.gradle modified successfully',
 	);
 	fs.writeFileSync(
 		workletsBuildGradlePath,
 		workletsBuildGradleNewContent,
-		"utf8",
+		'utf8',
 	);
 	console.info(
-		">> node_modules/react-native-worklets/android/build.gradle modified successfully",
+		'>> node_modules/react-native-worklets/android/build.gradle modified successfully',
 	);
 
-	console.info("> Success to fix ninja build, windows errors for long paths");
+	console.info('> Success to fix ninja build, windows errors for long paths');
 }
 
 main();
