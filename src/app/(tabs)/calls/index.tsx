7534661e6de6d24ee723f6c207babae05cb12b09
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2025-08-06 00:38:01 -0500

    feat: implement gestures handlers

--- src/app/(tabs)/calls/index.tsx
+++ src/app/(tabs)/calls/index.tsx
@@ -1,5 +1,6 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { format } from 'date-fns';
+import * as Haptics from 'expo-haptics';
 import { Stack } from 'expo-router';
 import { useState } from 'react';
 import {
@@ -13,23 +14,46 @@ import Animated, {
 	CurvedTransition,
 	FadeInUp,
 	FadeOutUp,
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming,
 } from 'react-native-reanimated';
 import { View } from '@/shared/ui/base';
 import { defaultStyles } from '@/shared/ui/default-styles';
 import { SegmentedControl } from '@/shared/ui/segmented-control';
+import SwipableRow from '@/shared/ui/swipable-row';
 import { colors } from '@/theme/colors';
 import calls from '../../../../assets/data/calls.json';
 
 const transition = CurvedTransition.delay(100);
+const AnimatedTouchableOpacity =
+	Animated.createAnimatedComponent(TouchableOpacity);
 
 export default function Calls() {
 	const [isEditing, setIsEditing] = useState(false);
 	const [selectedOption, setSelectedOption] = useState<'All' | 'Missed'>('All');
 	const [items, setItems] = useState(calls);
 
+	const editing = useSharedValue(-30);
+
 	const onEdit = () => {
-		setIsEditing((prev) => !prev);
+		const newEditingValue = !isEditing;
+		editing.value = newEditingValue ? 0 : -30;
+		setIsEditing(newEditingValue);
 	};
+
+	const removeCall = (id: string) => {
+		Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+		setItems((prev) => prev.filter((item) => item.id !== id));
+	};
+
+	const animatedRowStyles = useAnimatedStyle(() => ({
+		transform: [
+			{
+				translateX: withTiming(editing.value),
+			},
+		],
+	}));
 
 	return (
 		<View style={{ flex: 1, backgroundColor: colors.background }}>
@@ -84,59 +108,88 @@ export default function Calls() {
 						)}
 						itemLayoutAnimation={transition}
 						renderItem={({ item, index }) => (
-							<Animated.View
-								entering={FadeInUp.delay(index * 10)}
-								exiting={FadeOutUp}
+							<SwipableRow
+								onDelete={() => {
+									removeCall(item.id);
+								}}
 							>
-								<View style={defaultStyles.item}>
-									<Image source={{ uri: item.img }} style={styles.avatar} />
+								<Animated.View
+									entering={FadeInUp.delay(index * 10)}
+									exiting={FadeOutUp}
+									style={{
+										flexDirection: 'row',
+										alignItems: 'center',
+									}}
+								>
+									<AnimatedTouchableOpacity
+										onPress={() => removeCall(item.id)}
+										style={[
+											animatedRowStyles,
+											{
+												paddingLeft: 8,
+												backgroundColor: 'white',
+											},
+										]}
+									>
+										<Ionicons
+											name='remove-circle'
+											color={colors.red}
+											size={24}
+										/>
+									</AnimatedTouchableOpacity>
 
-									<View style={{ flex: 1, gap: 2 }}>
-										<Text
-											style={{
-												fontSize: 18,
-												color: item.missed ? colors.red : colors.textPrimary,
-											}}
-										>
-											{item.name}
-										</Text>
+									<Animated.View
+										style={[defaultStyles.item, animatedRowStyles]}
+									>
+										<Image source={{ uri: item.img }} style={styles.avatar} />
+
+										<View style={{ flex: 1, gap: 2 }}>
+											<Text
+												style={{
+													fontSize: 18,
+													color: item.missed ? colors.red : colors.textPrimary,
+												}}
+											>
+												{item.name}
+											</Text>
+
+											<View
+												style={{
+													flexDirection: 'row',
+													alignItems: 'center',
+													gap: 4,
+												}}
+											>
+												<Ionicons
+													name={item.video ? 'videocam' : 'call'}
+													size={16}
+													color={colors.gray}
+												/>
+												<Text style={{ color: colors.gray, flex: 1 }}>
+													{item.incoming ? 'Incoming' : 'Outgoing'}
+												</Text>
+											</View>
+										</View>
 
 										<View
 											style={{
+												gap: 6,
 												flexDirection: 'row',
 												alignItems: 'center',
-												gap: 4,
 											}}
 										>
-											<Ionicons
-												name={item.video ? 'videocam' : 'call'}
-												size={16}
-												color={colors.gray}
-											/>
-											<Text style={{ color: colors.gray, flex: 1 }}>
-												{item.incoming ? 'Incoming' : 'Outgoing'}
+											<Text style={{ color: colors.gray }}>
+												{format(item.date, 'MM.dd.yy')}
 											</Text>
+											<Ionicons
+												name='information-circle-outline'
+												size={24}
+												color={colors.primary}
+											/>
 										</View>
-									</View>
-
-									<View
-										style={{
-											gap: 6,
-											flexDirection: 'row',
-											alignItems: 'center',
-										}}
-									>
-										<Text style={{ color: colors.gray }}>
-											{format(item.date, 'MM.dd.yy')}
-										</Text>
-										<Ionicons
-											name='information-circle-outline'
-											size={24}
-											color={colors.primary}
-										/>
-									</View>
-								</View>
-							</Animated.View>
+									</Animated.View>
+								</Animated.View>
+							</SwipableRow>
 						)}
 					/>
 				</Animated.View>
---
+++ src/shared/ui/swipable-row.tsx
@@ -0,0 +1,70 @@
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import ReanimatedSwipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import Reanimated, {
+	type SharedValue,
+	useAnimatedStyle,
+} from 'react-native-reanimated';
+import { colors } from '@/theme/colors';
+
+const THRESHOLD = 100;
+
+function RightAction({
+	prog,
+	drag,
+	onDelete,
+}: {
+	prog: SharedValue<number>;
+	drag: SharedValue<number>;
+	onDelete: () => void;
+}) {
+	const styleAnimation = useAnimatedStyle(() => {
+		return {
+			transform: [{ translateX: drag.value + THRESHOLD }],
+		};
+	});
+
+	return (
+		<Reanimated.View style={styleAnimation}>
+			<TouchableOpacity onPress={onDelete} style={styles.rightAction}>
+				<Text style={styles.rightActionText}>Delete</Text>
+			</TouchableOpacity>
+		</Reanimated.View>
+	);
+}
+
+export default function SwipableRow({
+	children,
+	onDelete,
+}: {
+	children: React.ReactNode;
+	onDelete: () => void;
+}) {
+	return (
+		<ReanimatedSwipeable
+			containerStyle={styles.swipeable}
+			friction={2}
+			enableTrackpadTwoFingerGesture
+			rightThreshold={THRESHOLD}
+			renderRightActions={(prog, drag) => (
+				<RightAction prog={prog} drag={drag} onDelete={onDelete} />
+			)}
+		>
+			{children}
+		</ReanimatedSwipeable>
+	);
+}
+
+const styles = StyleSheet.create({
+	rightAction: {
+		width: THRESHOLD,
+		height: '100%',
+		backgroundColor: colors.red,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	rightActionText: {
+		color: colors.white,
+	},
+	swipeable: {},
+});
